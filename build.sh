4fdc5d6bd300786af2e51a305990a4d6b3fd3f17
ENKO <k@narukoshin.me>
2025-10-26 22:20:09 +0200

    building scripts for compiling core and plugins

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script is used to compile the main app binaries
+source ./build/build-core.sh
+
+# This script is used to compile the Proxmania plugin
+source ./build/build-proxmania.sh
